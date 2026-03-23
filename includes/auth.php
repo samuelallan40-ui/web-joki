f3d6a32bcffbcda9aaa88fbdacde66e72202a575
samuelallan40-ui <samuel.allan40@sma.belajar.id>
2026-03-23 16:26:18 +0700

    Create auth.php

---
+++ includes/auth.php
@@ -0,0 +1,21 @@
+<?php
+// includes/auth.php
+// Buat cek apakah user sudah login
+function isLoggedIn() {
+    return isset($_SESSION['user_id']);
+}
+
+function redirectIfNotLoggedIn() {
+    if (!isLoggedIn()) {
+        header('Location: login.php');
+        exit();
+    }
+}
+
+function redirectIfLoggedIn() {
+    if (isLoggedIn()) {
+        header('Location: dashboard.php');
+        exit();
+    }
+}
+?>
