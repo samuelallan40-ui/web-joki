47948470623e3fc67ebad60316ba59376590c66f
samuelallan40-ui <samuel.allan40@sma.belajar.id>
2026-03-23 16:27:02 +0700

    Create footer.php

---
+++ includes/footer.php
@@ -0,0 +1,17 @@
+<?php
+// includes/footer.php
+?>
+    </main>
+    <footer>
+        <div class="container">
+            <p>&copy; 2024 JokiML - All rights reserved</p>
+            <div class="social-links">
+                <a href="#"><i class="fab fa-discord"></i></a>
+                <a href="#"><i class="fab fa-whatsapp"></i></a>
+                <a href="#"><i class="fab fa-instagram"></i></a>
+            </div>
+        </div>
+    </footer>
+    <script src="assets/js/script.js"></script>
+</body>
+</html>
